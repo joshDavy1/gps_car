fb7feb11780fb8be304e2c9230788c0fa6ad736a
Hayden <hayden@haydencoe.co.uk>
2020-09-23 23:26:32 +0100

    Update robotStateCode.py

--- current/robotStateCode.py
+++ current/robotStateCode.py
@@ -1,40 +1,52 @@
-###############################
-#
-# File   : robotStateCode.py
-# Author : Hayden Coe
-# Date   : 13/09/2020
-#
-##############################
-
 from transitions import Machine
 import time
 
 class RobotState(object):
 
-    # Define some states. 
-    states = ['init', 'called']
+    # Define states. 
+    states = ['INIT', 'CALLED', 'ARRIVED', 'LOADED']
 
     def __init__(self):
 
         # Initialize the state machine
-        self.machine = Machine(model=self, states=RobotState.states, initial='init')
+        self.machine = Machine(model=self, states=RobotState.states, initial='INIT')
 
-        # Add some transitions. 
-        self.machine.add_transition(trigger='call_robot', source='init', dest='called', after='called_session')
+        # Transitions. 
+        self.machine.add_transition(trigger='call_robot', source='INIT', dest='CALLED', after='called_session')
 
-        self.machine.add_transition(trigger='cancel_robot', source='called', dest='init', after='canceled_session')
-
+        self.machine.add_transition(trigger='cancel_robot', source='CALLED', dest='INIT', after='canceled_session')
+        
+        self.machine.add_transition(trigger='cancel_load', source='ARRIVED', dest='INIT', after='canceled_load_session')
+       
+        self.machine.add_transition(trigger='robot_arrived', source='CALLED', dest='ARRIVED', after='arrived_session')
+        
+        self.machine.add_transition(trigger='robot_loaded', source='ARRIVED', dest='LOADED', after='loaded_session')
+        
+        self.machine.add_transition(trigger='user_reset', source='LOADED', dest='INIT', after='reset_session')
 
     def called_session(self):
         
         print("Now in state: " +  self.state);
         
-    
     def canceled_session(self):
         
         print("Now in state: " +  self.state);
+    
+    def canceled_load_session(self):
+        
+        print("Now in state: " +  self.state);
        
-
+    def arrived_session(self):
+        
+        print("Now in state: " +  self.state);
+        
+    def loaded_session(self):
+        
+        print("Now in state: " +  self.state);
+        
+    def reset_session(self):
+        
+        print("Now in state: " +  self.state);
         
         
         
